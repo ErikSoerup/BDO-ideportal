bb91a1d8771075d0d92a2a1d797d9245d4a4665a
Paul <paul@yakkuru.local>
2010-11-16 14:26:45 -0600

    Oops! Fixed migration.

--- db/migrate/20101116192641_update_default_current_id_in_user_roles.rb
+++ db/migrate/20101116192641_update_default_current_id_in_user_roles.rb
@@ -3,8 +3,8 @@ class UpdateDefaultCurrentIdInUserRoles < ActiveRecord::Migration
     old_default_current_id = 1
     Role.transaction do
       Role.find(:all, :conditions => { :authorizable_type => 'Current', :authorizable_id => old_default_current_id }).each do |role|
-        r.authorizable_id = Current::DEFAULT_CURRENT_ID
-        r.save!
+        role.authorizable_id = Current::DEFAULT_CURRENT_ID
+        role.save!
       end
     end
   end
