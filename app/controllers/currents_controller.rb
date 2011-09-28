4b9a3dd0ca8bfcc3ceb8f0d072aa975549f6bb38
Rajib Ahmed <cool_rajib@hotmail.com>
2011-09-28 03:54:36 +0200

    added translation files

--- app/controllers/currents_controller.rb
+++ app/controllers/currents_controller.rb
@@ -1,31 +1,31 @@
 class CurrentsController < ApplicationController
-  
+
   make_resourceful do
     actions :show, :index
-    
+
     before :show do
       @current_ideas = search_ideas(params.merge(:search => ['current', current_object.id]))
     end
-    
+
     response_for :show do |format|
       format.html
       #format.xml   # for future expansion
       # current/show rss currently supported via /ideas/search/current/<ID>?format=rss
     end
   end
-  
+
   def current_objects
     @currrents ||= Current.find(:all, :conditions=>"id != #{Current::DEFAULT_CURRENT_ID}")
   end
-  
+
   def page_title
     if @current
       @current.title
     else
-      "Idea Currents"
+      t("idea_current")
     end
   end
-  
+
   def change_subscription(enabled)
     @current = current_object
     if enabled
@@ -33,7 +33,7 @@ class CurrentsController < ApplicationController
     else
       @current.subscribers.delete current_user
     end
-    
+
     respond_to do |format|
       format.html do
         flash[:info] = 'You are no longer subscribed to notifications of new ideas on this current.' if !enabled
@@ -44,15 +44,15 @@ class CurrentsController < ApplicationController
       end
     end
   end
-  
+
   def subscribe
     change_subscription(true)
   end
-  
+
   def unsubscribe
     change_subscription(false)
   end
-  
+
   include ApplicationHelper
-  
+
 end
--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,9 +1,9 @@
 class TagsController < ApplicationController
-  
+
   def index
     @body_class = "tags-page"
   end
-  
+
   def current_objects
     unless @tags
       @tags = Tag.find_top_tags 100
@@ -15,16 +15,16 @@ class TagsController < ApplicationController
     @tags
   end
   helper_method :current_objects
-  
+
   def tag_style(tag, min_percent, max_percent)
     size = (Math.sqrt(tag.idea_count) - @min_idea_count) / (@max_idea_count - @min_idea_count)
     size = size * (max_percent - min_percent) + min_percent
     "font-size: #{size}%"
   end
   helper_method :tag_style
-  
+
   def page_title
-    "Idea Tags"
+    t("idea_tags")
   end
-  
+
 end
