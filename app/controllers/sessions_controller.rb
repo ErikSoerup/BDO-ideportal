fb785d639ab654f1c2c0bb7a84b919ffcbee08df
Rajib Ahmed <cool_rajib@hotmail.com>
2011-10-17 21:11:06 +0200

    updated the login views

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,20 +2,21 @@ require 'ostruct'
 
 # This controller handles login/logout functionality.
 class SessionsController < ApplicationController
-  
+
   include AuthenticatedSystem
   include TwitterHelper
 
   def new
     @body_class = 'login'
+    render :new , :layout=>false
   end
-  
+
   def new_twitter
     redirect_to twitter_auth_request_url(create_twitter_session_url)
   end
-  
+
   # No need face new_facebook here, because login initiation happens client-side via FB's Javascript API.
-  
+
   def create
     user = User.find_by_login(params[:email], params[:password])
     if user
@@ -30,18 +31,18 @@ class SessionsController < ApplicationController
       render :action => 'new'
     end
   end
-  
+
   def create_twitter
     credentials = verify_twitter_authorization
     unless credentials.blank?
       if user = User.find_by_twitter_handle(credentials.screen_name)
         self.current_user = user
-        
+
         # Credentials can change if user deauthorizes us, then reauthorizes
         user.twitter_token = twitter_oauth.access_token.token
         user.twitter_secret = twitter_oauth.access_token.secret
         user.save!
-        
+
         response_for_successful_login
       else
         redirect_to new_user_path(
@@ -56,22 +57,22 @@ class SessionsController < ApplicationController
       redirect_to login_path
     end
   end
-  
+
   def create_facebook
     if current_facebook_user
       user = User.find_by_facebook_uid(current_facebook_user.id)
       if user
         self.current_user = user
-      
+
         user.facebook_access_token = current_facebook_client.access_token
         user.save!
-      
+
         response_for_successful_login
       else
         # current_facebook_user only contains the uid, because it comes straight from the cookie.
         # To get the user's name & email, we need to talk to the FB servers.
         full_facebook_user = Mogli::User.find("me", current_facebook_client)
-        
+
         redirect_to new_user_path(
           :user => {
             :name  => full_facebook_user.name,
@@ -91,9 +92,9 @@ class SessionsController < ApplicationController
     flash[:info] = "You are now logged out. Thanks for using #{SHORT_SITE_NAME}!"
     redirect_back_or_default('/')
   end
-  
+
 private
-  
+
   def response_for_successful_login
     redirect_back_or_default do
       if current_user.active?
