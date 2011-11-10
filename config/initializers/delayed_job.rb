04801adabba009ad2c67852d62092e3a5309f6ad
Rajarshi Das <rajars_2576@yahoo.co.in>
2011-11-10 11:32:07 +0530

    small fix

--- config/initializers/delayed_job.rb
+++ config/initializers/delayed_job.rb
@@ -1,6 +1,16 @@
- # config/initializers/delayed_job_config.rb
-  Delayed::Job.destroy_failed_jobs = false
-  silence_warnings do
-    Delayed::Job.const_set("MAX_ATTEMPTS", 3)
-    Delayed::Job.const_set("MAX_RUN_TIME", 5.minutes)
-  end
+# Set to false to leave jobs in DB after they have exceed max_attempts
+Delayed::Worker.destroy_failed_jobs = true
+
+# Number of times to try a failed job before giving up. 15 attempts should spread the
+# attempts over approx 1.5 days.
+Delayed::Worker.max_attempts = 15
+
+# Timeout after which we presume that a worker has died, and another computer may pick
+# up its jobs. The worker is responsible for ensuring it take less time than this.
+# In the case of ShareIdeaJob, we wrap the worker in a Timeout::timeout.
+Delayed::Worker.max_run_time = 3.minutes
+
+# Time to sleep before querying for new jobs when work queue is empty.
+Delayed::Worker.sleep_delay = 8
+
+
