8a690bca33c276dc793b5f8a094c426508b3b9de
Rajarshi Das <rajars_2576@yahoo.co.in>
2011-11-10 12:22:05 +0530

    small fix

--- app/jobs/idea_posted_follower_job.rb
+++ app/jobs/idea_posted_follower_job.rb
@@ -1,5 +1,15 @@
-class IdeaPostedFollowerJob < Struct.new(:user, :idea)
+class IdeaPostedFollowerJob 
+
+attr_accessor :user_id, :comment_id
+  
+  def initialize(user, comment)
+    @user_id = user.id
+    @comment_id = comment.id
+  end
+
   def perform
+    user = User.find(user_id)
+    comment = Comment.find(comment_id)
     UserMailer.deliver_idea_posted_to_followers(user,idea)
   end    
 end  
