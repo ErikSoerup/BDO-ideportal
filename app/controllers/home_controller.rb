621c0286783601ba28986d7c98dc74f47c685a4c
ruby-dev-4 <ruby-dev-4@ruby-dev-4-desktop.(none)>
2011-11-15 12:37:24 +0530

    fix the department for advance page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -27,12 +27,12 @@ class HomeController < ApplicationController
       @ideas=current_user.ideas.paginate(:page => params[:page])
     elsif params[:val] == "current"
       @current_ideas = Current.paginate(:all, :conditions=>"id != #{Current::DEFAULT_CURRENT_ID}", :page => params[:page])
-    elsif Department.all.collect(&:name).include?(params[:val])
-      @dep= Department.find_by_id params[:val]
+    else
+      @dep= Department.find_by_id(params[:val])
        val=[]
         @dep.users.each do |user|
           val << user.ideas unless user.ideas.empty?
-          @idea={@dep.name => val} unless val.empty?
+          @idea={@dep.id => val} unless val.empty?
         end
       
       @ideas=@idea.values.first.first.paginate(:page => params[:page])
