84f4c6a2983600f810603819f48d4dac9ac74887
Rajarshi <solaiman@solaiman-laptop.(none)>
2012-01-27 15:44:23 +0530

    fix the helper

--- app/helpers/search_helper.rb
+++ app/helpers/search_helper.rb
@@ -22,7 +22,7 @@ module SearchHelper
   def search_ideas_sql(params)
     query_opts = {
       :page => params[:page] || 1,
-      :per_page => params[:page_size] || 8,
+      :per_page => params[:page_size] || 6,
       :include => [{:inventor => :postal_code}, :tags],
       :conditions => ['users.state = ? and ideas.hidden = ?', 'active', false] }
     @query_title = "Ideas"
@@ -153,7 +153,7 @@ public
   # Searches for an arbitrary model using a full-text tsearch query. Shared by public & admin UI.
   def search(current_model, params, sort = nil, order = true, &filter)
     page = (params[:page] || 1).to_i
-    page_size = (params[:per_page] || 8).to_i
+    page_size = (params[:per_page] || 6).to_i
 
     @search = current_model.find_by_tsearch(params[:search_text] || params[:search])
 
