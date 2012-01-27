27b1d752ba661ee461d088fb0a1eeb2d25640ee0
Rajarshi <solaiman@solaiman-laptop.(none)>
2012-01-27 14:26:46 +0530

    fix the comments count

--- public/javascripts/editjs.js
+++ public/javascripts/editjs.js
@@ -13,20 +13,7 @@
     set(el, { opacity: '0' });
   }
 
-  var el = document.createElement('a');
-
-  set(el, { opacity: 1, position: 'fixed', top: '-1px', right: '-1px', padding: '5px 10px', 'text-shadow': '0px 1px 1px #fff', 'border-top-right-radius': '5px', MozBorderRadiusBottomleft: '5px', border: '1px solid #999', textDecoration: 'none', font: '12px "Helvetica Neue", Arial, Helvetica', WebkitTransition: 'opacity ease-out 100ms', MozTransition: 'opacity ease-out 100ms', OTransition: 'opacity ease-out 100ms', transition: 'opacity ease-out 100ms' });
-
-  el.innerHTML = 'Edit in jsbin.com';
-  el.href = window.location.pathname + (window.location.pathname.substr(-1) == '/' ? '' : '/') + 'edit';
-
-  el.onmouseover = function () {
-    this.style.opacity = 1;
-  };
-
-  el.onmouseout = function () {
-    this.style.opacity = 0;
-  };
+  
 
   document.body.appendChild(el);
   setTimeout(hide, 2000);
