f4f7f5b9cd9d198c411b1e6c1e8656d9e7bbfecd
prajkta <prajkta.p@gmail.com>
2012-05-21 16:47:37 +0530

    When no tags are selected, the idea is tagged with "add tag"

--- public/javascripts/jquery.tagify.js
+++ public/javascripts/jquery.tagify.js
@@ -7,7 +7,7 @@
 			delimiters: [13],          // what user can type to complete a tag in char codes: [enter], [comma]
 			outputDelimiter: ',',           // delimiter for tags in original input field
 			cssClass: 'tagify-container',   // CSS class to style the tagify div and tags, see stylesheet
-			addTagPrompt: 'add tags',       // placeholder text
+			addTagPrompt: 'Tilføj emneord',       // placeholder text
 			addTagOnBlur: false				// Add a tag on blur when not empty
 		},
 		
