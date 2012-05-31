e037d761d0039d496bd6c863669b320c9d66bdaa
prajkta <prajkta.p@gmail.com>
2012-05-31 17:19:15 +0530

    added current ip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
     flash.clear
     @body_class = 'login'
     #    @ip_addr = request.ip
-    @ip_addr = request.ip == "127.0.0.1" || request.ip == "14.97.242.62" ? "83.151.150.86" : request.ip
+    @ip_addr = request.ip == "127.0.0.1" || request.ip == "14.97.236.203" ? "83.151.150.86" : request.ip
     if @ip_addr != "83.151.150.86"
       @message = "Du kan kun logge ind via det interne netværk. Log først på VPN og derefter på ideportalen."
       @authorized_access = false
